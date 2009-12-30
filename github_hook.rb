cd22660c5134467007fa50c56dcc470b1989080f
Jose Santos <jsmpereira@gmail.com>
2009-12-30 02:55:21 +0000

    Debug

--- github_hook.rb
+++ github_hook.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'oauth'
 require 'sinatra'
 require 'json'
-require 'pp'
 
 configure do
   begin
--- services/github.rb
+++ services/github.rb
@@ -7,9 +7,12 @@ service :github do |payload, project_alias|
   
   # Search for '[Closes #ticket_id]' in commit message
   ticket_id = message.scan(%r{Closes #+[0-9]+}).to_s.split("#")[1]  
-  pp @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
+  ticket = @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
+  
+  puts ticket
   
   comment_text = "Closed: #{commit_url}"	
-  pp @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
-
+  comment =  @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
+  
+  puts comment
 end
