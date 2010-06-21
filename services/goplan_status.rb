35c990459e34b5e187bd9196d806650b10566451
Jose Santos <jsmpereira@gmail.com>
2010-06-21 17:14:44 +0100

    Small tweak

--- services/goplan_status.rb
+++ services/goplan_status.rb
@@ -1,11 +1,10 @@
 service :goplan_status do |payload, project_alias|
   repository = payload['repository']['name']
-  message = payload['commits'][0]['message'][30]
   commit_url = payload['commits'][0]['url']
   tiny_url = shorten_url(commit_url)
   
   url = "/#{project_alias}/api/"
   
-  status_text = "[#{repository}] #{message} - #{tiny_url}"
+  status_text = "Commit: [#{repository}] #{tiny_url}"
   @access_token.put("#{url}statuses/update", {:format => 'json', :text => status_text})
 end
