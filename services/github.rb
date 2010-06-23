80b60a2aa685d5e4bf991ddf0169b2ffdb3c3e04
Jose Santos <jsmpereira@gmail.com>
2010-06-23 12:18:44 +0100

    Add support for closing of multiple tickets

--- services/github.rb
+++ services/github.rb
@@ -6,10 +6,14 @@ service :github do |payload, project_alias|
   url = "/#{project_alias}/api/"
   
   # Search for '[Closes #ticket_id]' in commit message
-  ticket_id = message.scan(%r{Closes #+[0-9]+}).to_s.split("#")[1]  
-  @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
+  tickets = message.scan(%r{Closes #+[0-9]+})
   
-  comment_text = "Closed: #{commit_url}"	
-  @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
+  tickets.each do |ticket|
+    ticket_id = ticket.to_s.split("#")[1]
+    @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
+    
+    comment_text = "Closed: #{commit_url}"	
+    @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
+  end
 
 end
