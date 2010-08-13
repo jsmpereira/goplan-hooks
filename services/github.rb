c127acdcbbd5420f9606ed6d9a2a6b93e3de8d1b
Jose Santos <jsmpereira@gmail.com>
2010-08-13 12:20:16 +0100

    Ticket related commits: add comment to ticket without closing it.

--- services/github.rb
+++ services/github.rb
@@ -5,15 +5,31 @@ service :github do |payload, project_alias|
   
   url = "/#{project_alias}/api/"
   
-  # Search for '[Closes #ticket_id]' in commit message
-  tickets = message.scan(%r{Closes #+[0-9]+})
+  # Search for '[Closes #ticket_id]' and '[Relates to #ticket_id]' in commit message
+  tickets = {}
+  tickets[:to_close] = message.scan(%r{Closes #+[0-9]+})}
+  tickets[:to_relate] = message.scan(%r{Relates to #+[0-9]+})}
   
-  tickets.each do |ticket|
-    ticket_id = ticket.to_s.split("#")[1]
-    @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
-    
-    comment_text = "Closed: #{commit_url}"	
-    @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
+  if !tickets[:to_close].empty?
+    tickets[:to_close].each do |ticket|
+
+      ticket_id = ticket.to_s.split("#")[1]
+      @access_token.put("#{url}tickets/update/#{ticket_id}.json", {'ticket[status]' => 2})
+
+      comment_text = "Closed: #{commit_url}"	
+
+      @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})      
+    end
+  end
+  
+  if !tickets[:to_relate].empty?
+    tickets[:to_relate].each do |ticket|
+
+      ticket_id = ticket.to_s.split("#")[1]
+      comment_text = "Relates to: #{commit_url}"	
+
+      @access_token.put("#{url}comments/create", {:format => 'json', 'comment[commentable_type]' => 'ticket', 'comment[commentable_cid]' => ticket_id, 'comment[text]' => comment_text})
+    end
   end
 
 end
